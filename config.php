530fa19d5b7574c6dec90431ac90f98eca968d5d
Navyush-RanaTx <navyush.rana@testingxperts.com>
2025-05-28 16:39:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,11 +7,11 @@ define('DB_PASSWORD', 'admin');
 define('DB_NAME', 'demo_db');
 
 // Attempt to connect to MySQL database //
-//$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
-//if($link === false){
- //   die("ERROR: Could not connect. " . mysqli_connect_error());
-//}
+ Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
 ?>
